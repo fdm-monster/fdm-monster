0b45aa2b84a184ad4279298ea2797e1ca006cdcf
David Zwart <davidzwa@gmail.com>
2021-09-11 15:31:49 +0200

    Script buttons instead of duplicating html

--- client_src/js/common/element.utils.js
+++ client_src/js/common/element.utils.js
@@ -2,6 +2,10 @@ export function elem(selector) {
   return document.getElementById(selector);
 }
 
+export function withId(label, id) {
+  return `${label}-${id}`;
+}
+
 export function addClick(selector, cb) {
   return elem(selector).addEventListener("click", cb);
 }
--- client_src/js/common/quick-action.elements.js
+++ client_src/js/common/quick-action.elements.js
@@ -1,68 +1,72 @@
 import { ACTIONS } from "./quick-action.constants";
+import { withId } from "./element.utils";
+
+function button({ id, link = false, classList, title, icon, customTags }) {
+  const titleTag = title ? `title="${title}"` : "";
+  const idTag = `id="${id}"`;
+  const tag = link ? "a" : "button";
+  const classTag = `class="${classList}"`;
+  const iconTag = `<i class="fas ${icon}"></i>`;
+
+  return `
+    <${tag}
+        ${idTag}
+        ${classTag}
+        ${titleTag}
+        type="button"
+        ${customTags || ""}
+    >
+        ${iconTag}
+    </${tag}>
+    `;
+}
 
 export function printerControlBtn(id) {
-  return `
-    <button  
-         title="Control Your Printer"
-         id="${ACTIONS.printerButton}-${id}"
-         type="button"
-         class="tag btn btn-primary btn-sm"
-         data-toggle="modal"
-         data-target="#printerManagerModal" disabled
-         >
-            <i class="fas fa-print"></i>
-    </button>
-    `;
+  return button({
+    title: "Control Your Printer",
+    id: withId(ACTIONS.printerButton, id),
+    classList: "tag btn btn-primary btn-sm",
+    icon: "fa-plug",
+    customTags: `data-toggle="modal" data-target="#printerManagerModal" disabled`
+  });
 }
 
 export function printerWebBtn(id, webURL) {
-  return `
-    <a title="Open OctoPrint"
-       id="${ACTIONS.printerWeb}-${id}"
-       type="button"
-       class="tag btn btn-info btn-sm"
-       target="_blank"
-       href="${webURL}" role="button"><i class="fas fa-globe-europe"></i></a>
-  `;
+  return button({
+    title: "Open OctoPrint",
+    link: true,
+    id: withId(ACTIONS.printerWeb, id),
+    classList: "tag btn btn-sm btn-info",
+    icon: "fa-globe-europe",
+    customTags: `target="_blank" href="${webURL}" role="button"`
+  });
 }
 
 export function printerReSyncBtn(id) {
-  return `
-    <button  
-             title="Re-Sync your printer"
-             id="${ACTIONS.printerSyncButton}-${id}"
-             type="button"
-             class="tag btn btn-success btn-sm"
-    >
-        <i class="fas fa-sync"></i>
-    </button>
-  `;
+  return button({
+    title: "Re-Sync your printer",
+    id: withId(ACTIONS.printerSyncButton, id),
+    classList: "tag btn btn-sm btn-success",
+    icon: "fa-sync"
+  });
 }
 
 export function printerQuickConnect(id) {
-  return `
-    <button  
-         title="Quickly connect/disconnect your printer"
-         id="${ACTIONS.printerQuickConnect}-${id}"
-         type="button"
-         class="tag btn btn-danger btn-sm"
-         >
-            <i class="fas fa-plug-off"></i>
-    </button>
-    `;
+  return button({
+    title: "Quickly connect/disconnect your printer",
+    id: withId(ACTIONS.printerQuickConnect, id),
+    classList: "tag btn btn-sm btn-danger",
+    icon: "fa-plug-off"
+  });
 }
 
 export function printerEnableToggle(id) {
-  return `
-    <button  
-         title="Enable/disable your printer in OctoFarm"
-         id="${ACTIONS.printerEnableToggle}-${id}"
-         type="button"
-         class="tag btn btn-danger btn-sm"
-         >
-            <i class="fas fa-toggle-off"></i>
-    </button>
-    `;
+  return button({
+    title: "Enable/disable your printer in OctoFarm",
+    id: withId(ACTIONS.printerEnableToggle, id),
+    classList: "tag btn btn-sm btn-danger",
+    icon: "fa-toggle-off"
+  });
 }
 
 export function powerBtnHolder(id) {
