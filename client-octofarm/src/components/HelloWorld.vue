a5e5dcf4e1b13044b8003e052841763b6bfaeaea
David Zwart <davidzwa@gmail.com>
2021-09-22 22:01:54 +0200

    Add login

--- client-octofarm/src/components/HelloWorld.vue
+++ client-octofarm/src/components/HelloWorld.vue
@@ -1,30 +1,16 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img :src="require('../assets/logo.png')" class="my-3" contain height="30" />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-col>
-    </v-row>
+    <Login></Login>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import Login from "@/components/Login.vue";
 
 export default Vue.extend({
   name: "HelloWorld",
-
-  data: () => ({
-  })
+  components: {Login},
+  data: () => ({})
 });
 </script>
--- client-octofarm/src/store/index.ts
+++ client-octofarm/src/store/index.ts
@@ -4,7 +4,9 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    serverSettings: {}
+  },
   mutations: {},
   actions: {},
   modules: {}
