1be78bfed143ed280134f03023d20c3755effab1
David Zwart <davidzwa@gmail.com>
2021-09-26 17:14:28 +0200

    Add search and fix name col

--- client-octofarm/src/views/Printers.vue
+++ client-octofarm/src/views/Printers.vue
@@ -1,41 +1,55 @@
 <template>
-  <v-data-table
-    :headers="dessertHeaders"
-    :items="printers"
-    :single-expand="true"
-    :expanded.sync="expanded"
-    class="elevation-1"
-    item-key="_id"
-    show-expand
-  >
-    <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>Expandable Table</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-switch v-model="singleExpand" class="mt-2" label="Single expand"></v-switch>
-      </v-toolbar>
-    </template>
-    <template v-slot:item.name="{ item }">
-      <v-chip
-          color="primary"
-          dark
-      >
-        {{ item.printerName || item.printerURL }}
-      </v-chip>
-    </template>
-    <template v-slot:item.group="{ item }">
-      <v-chip
-          v-if="item.group"
-          color="primary"
-          dark
-      >
-        {{ item.group }}
-      </v-chip>
-    </template>
-    <template v-slot:expanded-item="{ headers, item }">
-      <td :colspan="headers.length">More info about {{ JSON.stringify({_id:item._id, name:item.printerName, printerURL:item.printerURL}) }}</td>
-    </template>
-  </v-data-table>
+  <v-card>
+    <v-card-title>
+      Nutrition
+      <v-spacer></v-spacer>
+    <v-text-field
+        class="p-2"
+      v-model="search"
+      append-icon="mdi-magnify"
+      label="Search"
+      single-line
+      hide-details
+    ></v-text-field>
+    </v-card-title>
+
+    <v-data-table
+      :headers="dessertHeaders"
+      :items="printers"
+      :single-expand="true"
+      :expanded.sync="expanded"
+      :search="search"
+      class="elevation-1"
+      item-key="_id"
+      show-expand
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Expandable Table</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-switch v-model="singleExpand" class="mt-2" label="Single expand"></v-switch>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.printerName="{ item }">
+        <v-chip color="primary" dark>
+          {{ item.printerName || item.printerURL }}
+        </v-chip>
+      </template>
+      <template v-slot:item.group="{ item }">
+        <v-chip v-if="item.group" color="primary" dark>
+          {{ item.group }}
+        </v-chip>
+      </template>
+      <template v-slot:expanded-item="{ headers, item }">
+        <td :colspan="headers.length">
+          More info about
+          {{
+            JSON.stringify({ _id: item._id, name: item.printerName, printerURL: item.printerURL })
+          }}
+        </td>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -51,6 +65,7 @@ export default class Printers extends Vue {
   @Action loadPrinters: () => Promise<Printer[]>;
   @Getter printers: Printer[];
 
+  search = "";
   expanded = [];
   singleExpand = true;
   dessertHeaders = [
@@ -58,7 +73,7 @@ export default class Printers extends Vue {
       text: "Printer Name",
       align: "start",
       sortable: true,
-      value: "name"
+      value: "printerName"
     },
     { text: "Group", value: "group" },
     { text: "Enabled", value: "enabled" },
