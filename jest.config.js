f614cc65d90764fb7d347e53da0060c3ff799b1b
David Zwart <davidzwa@gmail.com>
2021-10-02 01:25:54 +0200

    Fix jest ignore, fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: "node",
   testTimeout: 5000,
-  modulePathIgnorePatterns: ["server_2x", "client-octofarm"],
+  modulePathIgnorePatterns: ["server_2x", "client-vue"],
   globalSetup: "./test/setup-global.js",
   setupFilesAfterEnv: ["./test/setup-after-env.js"]
 };
