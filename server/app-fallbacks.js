cda5a65a0c3c27f2760bdf507ae2516a9be23723
David Zwart <davidzwa@gmail.com>
2021-10-04 00:19:49 +0200

    Fix lock, fix tests with removed packages

--- server/app-fallbacks.js
+++ server/app-fallbacks.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
 const session = require("express-session");
 const flash = require("connect-flash");
 const { AppConstants } = require("./app.constants");
