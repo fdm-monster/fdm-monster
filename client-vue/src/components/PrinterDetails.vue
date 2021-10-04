351900f2194e05ada25fb8667767a0842a761743
David Zwart <davidzwa@gmail.com>
2021-10-04 21:15:26 +0200

    Add delete printer to Vue

--- client-vue/src/components/PrinterDetails.vue
+++ client-vue/src/components/PrinterDetails.vue
@@ -5,14 +5,17 @@
       JSON.stringify({
         _id: printer._id,
         name: printer.printerName,
-        printerURL: printer.printerURL
+        printerURL: printer.printerURL,
       })
     }}
-    <br />
+    <br/>
     Files:
     <v-btn @click="getFiles">Refresh</v-btn>
     <v-list>
-      <v-list-item></v-list-item>
+      <v-list-item v-for="file in printer.fileList.files" :key="file.name">
+        {{ file.name }} - {{ file.path }} - {{ file.date }}
+        <v-btn @click="deleteFile(file)">Delete</v-btn>
+      </v-list-item>
     </v-list>
   </div>
 </template>
@@ -20,9 +23,9 @@
 <script>
 import Component from "vue-class-component";
 import Vue from "vue";
-import { Prop } from "vue-property-decorator";
-import { Printer } from "@/models/printers/printer.model";
-import { OctoPrintService } from "@/backend";
+import {Prop} from "vue-property-decorator";
+import {Printer} from "@/models/printers/printer.model";
+import {OctoPrintService} from "@/backend";
 
 @Component({
   data: () => ({})
@@ -30,9 +33,17 @@ import { OctoPrintService } from "@/backend";
 export default class PrinterDetails extends Vue {
   @Prop(Printer) printer;
 
+  get printerId() {
+    return this.printer._id;
+  }
+
   async getFiles() {
-    const files = await OctoPrintService.getFiles(this.printer._id, false);
+    const files = await OctoPrintService.getFiles(this.printerId, false);
     console.log(files);
+  }
+
+  async deleteFile(file) {
+    await OctoPrintService.deleteFile(this.printerId, file.path);
   }
 }
 </script>
