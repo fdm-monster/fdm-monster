e9091aa0de03b76338615ab30e7f776c8326049e
Unknown <davidzwa@gmail.com>
2021-10-07 12:42:39 +0200

    Server update 400

--- test/api/server-update.test.js
+++ test/api/server-update.test.js
@@ -12,12 +12,12 @@ beforeAll(async () => {
 });
 
 describe("ServerUpdate Endpoint", () => {
-  it("should return 302 redirect when no user is logged in", async function () {
+  it("should return 400", async function () {
     process.env.npm_package_version = require("../../package.json").version;
     process.env.testlatest_package_version = require("../../package.json").version;
 
     const res = await request.post("/api/settings/server/update/server").send();
-    expect(res.statusCode).toEqual(302);
-    expect(res.text).toEqual("Found. Redirecting to /users/login");
+    expect(res.statusCode).toEqual(400);
+    // expect(res.text).toEqual("Found. Redirecting to /users/login");
   }, 10000);
 });
