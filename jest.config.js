a78aa0f68f25b4f0a3a4876cd921e0cda05898f5
Unknown <davidzwa@gmail.com>
2021-10-14 14:42:38 +0200

    Fix coverage ignore

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: "node",
   testTimeout: 5000,
-  modulePathIgnorePatterns: ["server-nest", "client-vue"],
+  modulePathIgnorePatterns: ["server-nest", "client-vue", "client-npm"],
   globalSetup: "./test/setup-global.js",
   setupFilesAfterEnv: ["./test/setup-after-env.js"],
   collectCoverageFrom: ["server/**/*.js"]
