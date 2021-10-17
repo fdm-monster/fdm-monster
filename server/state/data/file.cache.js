d501aa5cf07eece5ea423474f7441f6589a55e2d
David Zwart <davidzwa@gmail.com>
2021-10-17 19:49:13 +0200

    Remove smelly legacy

--- server/state/data/file.cache.js
+++ server/state/data/file.cache.js
@@ -1,7 +1,5 @@
 const _ = require("lodash");
 const Logger = require("../../handlers/logger.js");
-const { getUnits, getCost } = require("../../utils/spool.utils");
-const { getPrintCostNumeric } = require("../../utils/print-cost.util");
 const { getDefaultFileCleanStatistics } = require("../../constants/cleaner.constants");
 const { findFileIndex } = require("../../services/utils/find-predicate.utils");
 const { getFileCacheDefault } = require("../../constants/cache.constants");
@@ -126,114 +124,6 @@ class FileCache {
     fileList.files.splice(fileIndex, 1);
 
     return Status.success("File was removed");
-  }
-
-  // TODO convert this to a toFlat() implementation
-  // Currently unused
-  saveFileList(printerID, fileList, printerCostSettings, selectedFilament) {
-    // TODO check if exists and create if not
-    const printerFileStorage = this.#printerFileStorage[printerID];
-
-    const sortedFileList = [];
-    if (!!fileList?.files) {
-      for (let file of fileList.files) {
-        const fileToolUnits = getUnits(selectedFilament, file.length) || "";
-        const sortedFile = {
-          path: file.path,
-          fullPath: file.fullPath,
-          display: file.display,
-          name: file.name,
-          uploadDate: file.date,
-          fileSize: file.size,
-          thumbnail: file.thumbnail,
-          success: file.success,
-          failed: file.failed,
-          last: file.last,
-          // TODO the props below are 'extra'
-          expectedPrintTime: file.time,
-          // TODO these have no place in a file cache - derivatives
-          printCost: getPrintCostNumeric(file.time, printerCostSettings),
-          toolUnits: fileToolUnits,
-          toolCosts: getCost(selectedFilament, fileToolUnits) || ""
-        };
-
-        sortedFileList.push(sortedFile);
-      }
-    }
-
-    printerFileStorage.fileList = {
-      fileList: sortedFileList,
-      folderList: fileList.folders || [],
-      folderCount: fileList.folderCount || 0
-    };
-  }
-
-  // TODO
-  statistics(farmPrinters) {
-    const storageFree = [];
-    const storageTotal = [];
-    const devices = [];
-    const fileSizes = [];
-    const fileLengths = [];
-    const fileCount = [];
-    const folderCount = [];
-
-    // Collect unique devices - Total for farm storage should not duplicate storage on instances running on same devices.
-    farmPrinters.forEach((printer, index) => {
-      if (!!printer.storage) {
-        const device = {
-          ip: printer.printerURL,
-          index: printer.index,
-          storage: printer.storage
-        };
-        devices.push(device);
-      }
-      if (!!printer.fileList) {
-        printer.fileList.files?.forEach((file) => {
-          if (!isNaN(file.size)) {
-            fileSizes.push(file.size);
-          }
-          if (!isNaN(file.length)) {
-            fileLengths.push(file.length / 1000);
-          }
-
-          fileCount.push(file);
-        });
-        printer.fileList.folders?.forEach((file) => {
-          folderCount.push(file);
-        });
-      }
-    });
-
-    const uniqueDevices = _.uniqBy(devices, "printerURL");
-    uniqueDevices.forEach((device) => {
-      storageFree.push(device.storage.free);
-      storageTotal.push(device.storage.total);
-    });
-
-    const storageFreeTotal = storageFree.reduce((a, b) => a + b, 0);
-    const storageTotalTotal = storageTotal.reduce((a, b) => a + b, 0);
-    this.#fileStatistics.storageUsed = storageTotalTotal - storageFreeTotal;
-    this.#fileStatistics.storageTotal = storageTotalTotal;
-    this.#fileStatistics.storageRemain = storageFreeTotal;
-    this.#fileStatistics.storagePercent =
-      storageTotalTotal === 0
-        ? 0
-        : Math.floor((this.#fileStatistics.storageUsed / storageTotalTotal) * 100);
-    this.#fileStatistics.fileCount = fileCount.length;
-    this.#fileStatistics.folderCount = folderCount.length;
-    // TODO repeated calls?
-    if (fileSizes.length !== 0) {
-      this.#fileStatistics.biggestFile = Math.max(...fileSizes);
-      this.#fileStatistics.smallestFile = Math.min(...fileSizes);
-      this.#fileStatistics.averageFile = fileSizes.reduce((a, b) => a + b, 0) / fileCount.length;
-    }
-    if (fileLengths.length !== 0) {
-      this.#fileStatistics.biggestLength = Math.max(...fileLengths);
-      this.#fileStatistics.smallestLength = Math.min(...fileLengths);
-      this.#fileStatistics.averageLength =
-        fileLengths.reduce((a, b) => a + b, 0) / fileCount.length;
-    }
   }
 }
 
