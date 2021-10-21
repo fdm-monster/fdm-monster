2dffb2ae9e87660d2bbd19f8df2f6748bab6e165
David Zwart <davidzwa@gmail.com>
2021-10-21 12:03:52 +0200

    Add today and weekend countdown

--- client-vue/src/components/Generic/TopBar.vue
+++ client-vue/src/components/Generic/TopBar.vue
@@ -1,19 +1,35 @@
 <template>
   <v-app-bar app color="primary" dark>
     <v-toolbar-title class="text-uppercase white--text">
-      <span class="font-weight-light">MTB3D </span>
-      <span>Printer Manager</span>
+      <span class="font-weight-light">3D Print Far</span>
+      <strong>MTB3D</strong>
+      <small class="ml-12"> {{ currentTimeDiff || getWorkTimeDiff() }} TODAY</small>
+      <small class="ml-6"> {{ currentWeekendCountDown }} WEEKEND</small>
     </v-toolbar-title>
-
     <v-spacer></v-spacer>
 
-    <PrintJobsMenu />
+    <PrintJobsMenu/>
   </v-app-bar>
 </template>
 
 <script>
 import PrintJobsMenu from "@/components/Generic/PrintJobsMenu.vue";
-export default {
-  components: { PrintJobsMenu }
-};
+import Component from "vue-class-component";
+import Vue from "vue";
+import { DateTime } from "luxon";
+
+@Component({
+  components: {PrintJobsMenu}
+})
+export default class TopBar extends Vue {
+  currentTimeDiff = "";
+  currentWeekendCountDown = "";
+
+  getWorkTimeDiff() {
+    setInterval(() => {
+      this.currentWeekendCountDown = DateTime.now().startOf("week").set({weekDay: 5, hour: 18}).diffNow().toFormat("dD h:mm:ss");
+      this.currentTimeDiff = DateTime.fromObject({hour: 18}).diffNow().toFormat("h:mm:ss");
+    }, 1000);
+  }
+}
 </script>
