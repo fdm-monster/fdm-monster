c7279b8af4acf525d0938cdba00912a305a14f29
David Zwart <davidzwa@gmail.com>
2021-10-22 13:09:39 +0200

    Test create, update, delete of Alert, remove useless props of printer model

--- test/api/alert.test.js
+++ test/api/alert.test.js
@@ -13,6 +13,7 @@ let request;
 const alertsRoute = AppConstants.apiRoute + "/alert";
 const getRoute = alertsRoute;
 const createRoute = alertsRoute;
+const updateRoute = (id) => `${alertsRoute}/${id}`;
 
 beforeAll(async () => {
   await dbHandler.connect();
@@ -22,8 +23,29 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
-  Alert.deleteMany({});
+  return Alert.deleteMany({});
 });
+
+function getNormalAlert(printerIdArray) {
+  return {
+    active: true,
+    printer: printerIdArray,
+    trigger: "tomorrow",
+    message: "temperature hit the fan",
+    scriptLocation: "../safe_location/script.js"
+  };
+}
+
+async function createNormalAlert(request, printerIdArray) {
+  const response = await request.post(createRoute).send(getNormalAlert(printerIdArray));
+
+  expectOkResponse(response, {
+    active: true,
+    printer: printerIdArray
+  });
+
+  return response.body;
+}
 
 describe("AlertController", () => {
   it("should return empty alert list", async function () {
@@ -36,17 +58,30 @@ describe("AlertController", () => {
 
   it("should create new alert", async function () {
     const testPrinter = await createTestPrinter(request);
-    const response = await request.post(createRoute).send({
-      active: true,
-      printer: [testPrinter.id],
-      trigger: "tomorrow",
-      message: "temperature hit the fan",
-      scriptLocation: "../safe_location/script.js"
-    });
+    await createNormalAlert(request, [testPrinter.id]);
+  });
+
+  it("should update existing alert", async function () {
+    const testPrinter = await createTestPrinter(request);
+    const alert = await createNormalAlert(request, [testPrinter.id]);
+
+    const data = {
+      ...alert,
+      active: false
+    };
+    const response = await request.put(updateRoute(alert._id)).send(data);
 
     expectOkResponse(response, {
-      active: true,
-      printer: [testPrinter.id]
+      active: false
     });
+  });
+
+  it("should delete existing alert", async function () {
+    const testPrinter = await createTestPrinter(request);
+    const alert = await createNormalAlert(request, [testPrinter.id]);
+
+    const response = await request.delete(updateRoute(alert._id)).send();
+
+    expectOkResponse(response);
   });
 });
