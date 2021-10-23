442d7ca1755a9cf43c468516fa39ce8764a3172c
David Zwart <davidzwa@gmail.com>
2021-10-23 12:35:35 +0200

    Remove amialive and cover in app controller

--- server/controllers/app.controller.js
+++ server/controllers/app.controller.js
@@ -1,17 +1,20 @@
 const { createController } = require("awilix-express");
 const { AppConstants } = require("../app.constants");
+const isDocker = require("is-docker");
+const { isNodemon, isNode, isPm2 } = require("../utils/env.utils");
+const { ensureCurrentUserAndGroup } = require("../middleware/users");
 
 class AppController {
   #serverVersion;
   #settingsStore;
   #printersStore;
-  #serverPageTitle;
+  #serverUpdateService;
 
-  constructor({ settingsStore, printersStore, serverVersion, serverPageTitle }) {
+  constructor({ settingsStore, printersStore, serverVersion, serverUpdateService }) {
     this.#settingsStore = settingsStore;
     this.#serverVersion = serverVersion;
     this.#printersStore = printersStore;
-    this.#serverPageTitle = serverPageTitle;
+    this.#serverUpdateService = serverUpdateService;
   }
 
   welcome(req, res) {
@@ -28,11 +31,30 @@ class AppController {
       });
     }
   }
+
+  async getVersion(req, res) {
+    let softwareUpdateNotification = this.#serverUpdateService.getUpdateNotificationIfAny();
+
+    // ensure update_available can only be true when Administrator group found
+    if (req?.user?.group !== "Administrator") {
+      softwareUpdateNotification.update_available = false;
+    }
+
+    res.json({
+      isDockerContainer: isDocker(),
+      isNodemon: isNodemon(),
+      isNode: isNode(),
+      isPm2: isPm2(),
+      os: process.env.OS,
+      update: softwareUpdateNotification
+    });
+  }
 }
 
 // prettier-ignore
 module.exports = createController(AppController)
-  .prefix(AppConstants.apiRoute +"/")
-  .before([])
-  //.get("wizard, "wizard")
-  .get("", "welcome");
+    .prefix(AppConstants.apiRoute + "/")
+    .before([ensureCurrentUserAndGroup])
+    //.get("wizard, "wizard")
+    .get("", "welcome")
+    .get("version", "getVersion");
--- test/api/app.test.js
+++ test/api/app.test.js
@@ -7,8 +7,9 @@ const { expectOkResponse } = require("../extensions");
 
 let request;
 
-const welcomeRoute = AppConstants.apiRoute + "/";
+const welcomeRoute = AppConstants.apiRoute;
 const getRoute = welcomeRoute;
+const versionRoute = `${welcomeRoute}/version`;
 
 beforeAll(async () => {
   await dbHandler.connect();
@@ -18,12 +19,23 @@ beforeAll(async () => {
 describe("AppController", () => {
   it("should return welcome", async function () {
     const response = await request.get(getRoute).send();
-
     expect(response.body).toMatchObject({
       message:
         "Login not required. Please load UI instead by requesting any route with text/html Content-Type"
     });
+    expectOkResponse(response);
+  });
 
+  it("should return version", async function () {
+    const response = await request.get(versionRoute).send();
+    expectOkResponse(response, {
+      isDockerContainer: false,
+      isNode: false,
+      isPm2: false,
+      update: {
+        air_gapped: null
+      }
+    });
     expectOkResponse(response);
   });
 });
