f942f2839e48c63c5824fe16254e651603ff6bc5
David Zwart <davidzwa@gmail.com>
2022-02-26 10:47:32 +0100

    Cover settings API fully

--- server/test/api/settings-controller.test.js
+++ server/test/api/settings-controller.test.js
@@ -1,11 +1,11 @@
 const dbHandler = require("../db-handler");
 const { setupTestApp } = require("../test-server");
-const DITokens = require("../../container.tokens");
 const { AppConstants } = require("../../server.constants");
 const { expectOkResponse } = require("../extensions");
 const {
   printerFileCleanSettingKey,
-  getDefaultPrinterFileCleanSettings, getDefaultSettings
+  getDefaultPrinterFileCleanSettings,
+  getDefaultSettings
 } = require("../../constants/server-settings.constants");
 
 let request;
@@ -21,8 +21,20 @@ beforeAll(async () => {
 });
 
 describe("SettingsController", () => {
+  const newClientSettings = {};
+  const newServerSettings = {
+    [printerFileCleanSettingKey]: {
+      autoRemoveOldFilesBeforeUpload: true
+    }
+  };
   it("should OK on GET client-settings", async () => {
     const response = await request.get(clientRoute).send();
+    expect(response.body).not.toBeNull();
+    expectOkResponse(response);
+  });
+
+  it("should OK on PUT client-settings", async () => {
+    const response = await request.put(clientRoute).send(newClientSettings);
     expect(response.body).not.toBeNull();
     expectOkResponse(response);
   });
@@ -32,7 +44,16 @@ describe("SettingsController", () => {
     expect(response.body).not.toBeNull();
     expect(response.body).toMatchObject(getDefaultSettings());
     expect(response.body.server.registration).toBeTruthy();
-    expect(response.body[printerFileCleanSettingKey]).toMatchObject(getDefaultPrinterFileCleanSettings());
+    expect(response.body[printerFileCleanSettingKey]).toMatchObject(
+      getDefaultPrinterFileCleanSettings()
+    );
+    expectOkResponse(response);
+  });
+
+  it("should OK on PUT server-settings", async () => {
+    const response = await request.put(serverRoute).send(newServerSettings);
+    expect(response.body).not.toBeNull();
+    expect(response.body).toMatchObject(newServerSettings);
     expectOkResponse(response);
   });
 });
