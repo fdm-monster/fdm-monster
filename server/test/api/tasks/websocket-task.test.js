479cbc39cfab695f128ff2b3730060fff94f6830
Unknown <davidzwa@gmail.com>
2022-04-15 14:32:54 +0200

    Add websocket task test for big test coverage bump

---
+++ server/test/api/tasks/websocket-task.test.js
@@ -0,0 +1,41 @@
+const dbHandler = require("../../db-handler");
+const { setupTestApp } = require("../../test-server");
+const DITokens = require("../../../container.tokens");
+const { validNewPrinterState } = require("../../application/test-data/printer.data");
+
+let container;
+let taskManager;
+let printersStore;
+let serverTasks;
+let task;
+
+beforeAll(async () => {
+  await dbHandler.connect();
+  ({ container } = await setupTestApp(true));
+  taskManager = container.resolve(DITokens.taskManagerService);
+  printersStore = container.resolve(DITokens.printersStore);
+  task = container.resolve(DITokens.printerWebsocketTask);
+  serverTasks = container.resolve(DITokens.serverTasks);
+});
+
+describe("PrinterWebsocketTask", () => {
+  it("should try running websocket task", async () => {
+    try {
+      await task.run();
+    } catch (e) {
+      // It throws because system task has not been registered by the BootTask yet - complete fine
+      expect(e.toString()).toEqual(
+        "JobValidationError [anonymous]: The requested task with ID printerSystemTask was not registered"
+      );
+    }
+  });
+  it("should try connecting to OctoPrint websocket", async () => {
+    const newPrinterState = await printersStore.addPrinter(validNewPrinterState);
+
+    try {
+      await task.setupPrinterConnection(newPrinterState);
+    } catch (e) {
+      expect(e.toString()).toEqual("Error: OctoPrint apiKey was rejected. (Not retried)");
+    }
+  });
+});
