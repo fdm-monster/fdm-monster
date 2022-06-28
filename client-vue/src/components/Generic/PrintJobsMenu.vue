91688bbb638764363c446bb02af11103a326853e
Unknown <davidzwa@gmail.com>
2022-06-28 11:14:59 +0200

    Small typo

--- client-vue/src/components/Generic/PrintJobsMenu.vue
+++ client-vue/src/components/Generic/PrintJobsMenu.vue
@@ -43,7 +43,7 @@
         <v-divider></v-divider>
 
         <v-list>
-          <v-list-item v-if="!activePrintCount"> No active prints</v-list-item>
+          <v-list-item v-if="!activePrintCount"> No results</v-list-item>
           <v-list-item v-for="printer of activePrintJobs" :key="printer.id">
             <v-list-item-action>
               <v-progress-circular
