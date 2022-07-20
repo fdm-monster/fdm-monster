0d3045176989738657ba7a3d5322fa393866b77a
David Zwart <davidzwa@gmail.com>
2022-07-20 11:48:36 +0200

    Remove unused margin bottom statement

--- client-vue/src/views/HomePrinterGrid.vue
+++ client-vue/src/views/HomePrinterGrid.vue
@@ -16,7 +16,7 @@
       </v-btn>
     </v-toolbar>
 
-    <v-banner v-drop-upload="{ printers: selectedPrinters }" style="margin-bottom: 0">
+    <v-banner v-drop-upload="{ printers: selectedPrinters }">
       <v-row style="margin-bottom: -20px">
         <v-col>
           <v-btn
