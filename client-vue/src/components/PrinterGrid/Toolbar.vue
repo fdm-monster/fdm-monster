0d293b4794c6f5c724316adb3da59509a52c3526
David Zwart <davidzwa@gmail.com>
2022-09-13 13:44:27 +0200

    chore: move out toolbar, start adding floor switcher

---
+++ client-vue/src/components/PrinterGrid/Toolbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-toolbar flat>
+    <v-toolbar-title>Location Map</v-toolbar-title>
+    <v-btn-toggle class="ml-7">
+      <v-btn>
+        <v-icon>format_align_left</v-icon>
+        Floor 1
+      </v-btn>
+
+      <v-btn>
+        <v-icon>format_align_center</v-icon>
+        Floor 2
+      </v-btn>
+    </v-btn-toggle>
+
+    <v-spacer></v-spacer>
+    <div>
+      <v-switch v-model="autoPrint" disabled hide-details label="Auto-select and print"></v-switch>
+    </div>
+    <v-btn class="ml-3" color="primary" type="button" @click="openCreatePrinterDialog()">
+      Create Printer
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import { printersState } from "@/store/printers.state";
+
+@Component({})
+export default class Toolbar extends Vue {
+  autoPrint = true;
+
+  openCreatePrinterDialog() {
+    printersState.setCreateDialogOpened(true);
+  }
+}
+</script>
