d1b3fc7a8b209fd913a0af86092fe25cc6c4adea
David Zwart <davidzwa@gmail.com>
2022-10-06 16:42:37 +0200

    test: cover PSTATE possibilities

--- server/test/definition/state-constant.test.js
+++ server/test/definition/state-constant.test.js
@@ -2,7 +2,8 @@ const {
   remapOctoPrintState,
   OP_STATE,
   mapStateToColor,
-  PSTATE
+  PSTATE,
+  CATEGORY
 } = require("../../constants/state.constants");
 
 describe("StateConstants-Mapping", function () {
@@ -44,8 +45,26 @@ describe("StateConstants-Mapping", function () {
   });
 
   it("should map illegal state to color", () => {
-    expect(mapStateToColor(null).category).toBe("Active");
-    expect(mapStateToColor(undefined).category).toBe("Active");
-    expect(mapStateToColor(PSTATE.OfflineAfterError).category).toBe("Error");
+    expect(mapStateToColor(null).category).toBe(CATEGORY.Active);
+    expect(mapStateToColor(undefined).category).toBe(CATEGORY.Active);
+    expect(mapStateToColor(PSTATE.OfflineAfterError).category).toBe(CATEGORY.Error);
+    expect(mapStateToColor(PSTATE.Loading).category).toBe(CATEGORY.Idle);
+    expect(mapStateToColor(PSTATE.Operational).category).toBe(CATEGORY.Idle);
+    expect(mapStateToColor(PSTATE.Paused).category).toBe(CATEGORY.Idle);
+    expect(mapStateToColor(PSTATE.Printing).category).toBe(CATEGORY.Active);
+    expect(mapStateToColor(PSTATE.Pausing).category).toBe(CATEGORY.Active);
+    expect(mapStateToColor(PSTATE.Cancelling).category).toBe(CATEGORY.Active);
+    expect(mapStateToColor(PSTATE.Starting).category).toBe(CATEGORY.Active);
+    expect(mapStateToColor(PSTATE.Disabled).category).toBe(CATEGORY.Disabled);
+    expect(mapStateToColor(PSTATE.Offline).category).toBe(CATEGORY.Offline);
+    expect(mapStateToColor(PSTATE.Searching).category).toBe(CATEGORY.Offline);
+    expect(mapStateToColor(PSTATE.NoAPI).category).toBe(CATEGORY.Offline);
+    expect(mapStateToColor(PSTATE.Shutdown).category).toBe(CATEGORY.Offline);
+    expect(mapStateToColor(PSTATE.Disconnected).category).toBe(CATEGORY.Disconnected);
+    expect(mapStateToColor(PSTATE.Complete).category).toBe(CATEGORY.Complete);
+    expect(mapStateToColor(PSTATE.ApiKeyRejected).category).toBe(CATEGORY.Error);
+    expect(mapStateToColor(PSTATE.GlobalAPIKey).category).toBe(CATEGORY.Error);
+    expect(mapStateToColor(PSTATE.Error).category).toBe(CATEGORY.Error);
+    expect(mapStateToColor(PSTATE.OfflineAfterError).category).toBe(CATEGORY.Error);
   });
 });
