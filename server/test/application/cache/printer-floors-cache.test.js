4dbbc8c85ff79512ca2d99faa07d44c7f713d3ef
David Zwart <davidzwa@gmail.com>
2022-10-13 14:08:37 +0200

    cover: cache edge case functionality

---
+++ server/test/application/cache/printer-floors-cache.test.js
@@ -0,0 +1,29 @@
+const dbHandler = require("../../db-handler");
+const { configureContainer } = require("../../../container");
+const DITokens = require("../../../container.tokens");
+const PrinterFloor = require("../../../models/PrinterFloor");
+
+let container;
+let printerFloorsCache;
+let Model = PrinterFloor;
+
+beforeAll(async () => {
+  await dbHandler.connect();
+  container = configureContainer();
+  printerFloorsCache = container.resolve(DITokens.printerFloorsCache);
+});
+
+beforeEach(async () => {
+  Model.deleteMany({});
+});
+
+describe(DITokens.printerFloorsCache, () => {
+  it("should throw on getting non-existing floor", async function () {
+    await expect(() =>
+      printerFloorsCache.getFloor("63452115122876ea11cd1656")
+    ).rejects.toBeDefined();
+  });
+  it("should delete floor", async function () {
+    await printerFloorsCache.delete("63452115122876ea11cd1656");
+  });
+});
