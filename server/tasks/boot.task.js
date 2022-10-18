7f08bd87cd7642a34d8b9401c8f5324e6b0d8c40
David Zwart <davidzwa@gmail.com>
2022-10-18 12:11:45 +0200

    Revert "fix: halted/slowdown docker build due to date-fns"
    This reverts commit ad3abd4a16974e8fd989666c198bcf9df2b5e604.

--- server/tasks/boot.task.js
+++ server/tasks/boot.task.js
@@ -75,6 +75,7 @@ class BootTask {
   async run(bootTaskScheduler = false) {
     try {
       await this.createConnection();
+      await this.migrateDatabase();
     } catch (e) {
       if (e instanceof MongooseError) {
         // Tests should just continue
@@ -141,6 +142,10 @@ class BootTask {
         "Created admin account as it was missing. Please consult the documentation for credentials."
       );
     }
+  }
+
+  async migrateDatabase() {
+    await runMigrations(mongoose.connection.db, mongoose.connection.getClient());
   }
 }
 
