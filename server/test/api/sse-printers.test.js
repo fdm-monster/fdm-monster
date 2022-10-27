aa5e0618cf1f0e6136875a5e2a3ee14afbecbe94
David Zwart <davidzwa@gmail.com>
2022-10-27 14:15:07 +0200

    test: cover multiple SSE clients

--- server/test/api/sse-printers.test.js
+++ server/test/api/sse-printers.test.js
@@ -3,6 +3,7 @@ const dbHandler = require("../db-handler");
 const { setupTestApp } = require("../test-server");
 const DITokens = require("../../container.tokens");
 const { AppConstants } = require("../../server.constants");
+const { delay } = require("lodash");
 
 let request;
 let container;
@@ -27,6 +28,11 @@ describe("SSE-Printers", () => {
 
     await new Promise(async (resolve) => {
       const es = new EventSource(url);
+      const es2 = new EventSource(url);
+
+      es2.onmessage = () => {
+        es2.close();
+      };
 
       es.onmessage = (e) => {
         firedEvent = true;
@@ -43,10 +49,12 @@ describe("SSE-Printers", () => {
           outletCurrentValues: expect.anything(),
         });
         es.close();
-        resolve();
       };
 
-      await sseTask.run();
+      delay(async () => {
+        await sseTask.run();
+        resolve();
+      }, 100);
     });
 
     expect(firedEvent).toBeTruthy();
