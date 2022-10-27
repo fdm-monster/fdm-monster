b4c842975eaa920719a680f2188a6fc43ab2c661
David Zwart <davidzwa@gmail.com>
2022-10-27 14:05:00 +0200

    test: cover history sse

---
+++ server/test/api/sse-history.test.js
@@ -0,0 +1,55 @@
+const EventSource = require("eventsource");
+const dbHandler = require("../db-handler");
+const { setupTestApp } = require("../test-server");
+const DITokens = require("../../container.tokens");
+const { AppConstants } = require("../../server.constants");
+const { octoPrintWebsocketEvent } = require("../../constants/event.constants");
+const { delay } = require("lodash");
+
+let request;
+let container;
+let eventEmitter;
+let sseTask;
+const defaultRoute = `${AppConstants.apiRoute}/history`;
+const sseRoute = `${defaultRoute}/sse`;
+
+beforeAll(async () => {
+  await dbHandler.connect();
+  ({ request, container } = await setupTestApp(true));
+
+  eventEmitter = container.resolve(DITokens.eventEmitter2);
+  sseTask = container.resolve(DITokens.printEventsSseTask);
+});
+
+describe("SSE-History", () => {
+  it("should be able to be called with an EventSource", async () => {
+    const getRequest = request.get(sseRoute);
+    const url = getRequest.url;
+    expect(url).toBeTruthy();
+
+    let firedEvent = false;
+
+    // Bind listener
+    await sseTask.run();
+
+    await new Promise(async (resolve) => {
+      const es = new EventSource(url);
+
+      es.onmessage = (e) => {
+        firedEvent = true;
+
+        let parsedMsg = JSON.parse(e.data);
+        expect(parsedMsg).toMatchObject({});
+        es.close();
+        resolve();
+      };
+
+      // Emit fake event asynchronously
+      delay(async () => {
+        await eventEmitter.emit(octoPrintWebsocketEvent("someId"), {});
+      }, 100);
+    });
+
+    expect(firedEvent).toBeTruthy();
+  }, 10000);
+});
