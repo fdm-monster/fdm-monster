42d8ca07a06ee0eaaf7cf06d68240247f4af4f62
David Zwart <davidzwa@gmail.com>
2022-11-01 11:54:56 +0100

    chore: remove legacy settings

--- server/constants/server-settings.constants.js
+++ server/constants/server-settings.constants.js
@@ -1,10 +1,5 @@
 const { AppConstants } = require("../server.constants");
 
-// Default Settings
-const onlinePolling = {
-  seconds: 0.5,
-};
-
 const server = {
   port: AppConstants.defaultServerPort,
   uploadFolder: AppConstants.defaultFileStorageFolder,
@@ -26,42 +21,15 @@ const getDefaultPrinterFileCleanSettings = () => ({
   autoRemoveOldFilesCriteriumDays: 7,
 });
 
-const HISTORY_SETTINGS = {
-  snapshot: "snapshot",
-  thumbnails: "thumbnails",
-  timelapse: "timelapse",
-};
-
-const history = {
-  [HISTORY_SETTINGS.snapshot]: {
-    onFailure: true,
-    onComplete: true,
-  },
-  [HISTORY_SETTINGS.thumbnails]: {
-    onFailure: true,
-    onComplete: true,
-  },
-  [HISTORY_SETTINGS.timelapse]: {
-    onFailure: false,
-    onComplete: false,
-    deleteAfter: false,
-  },
-};
-
 const getDefaultSettings = () => ({
-  onlinePolling,
   server,
   [printerFileCleanSettingKey]: getDefaultPrinterFileCleanSettings(),
   timeout: getDefaultTimeout(),
-  history,
 });
 
 module.exports = {
-  HISTORY_SETTINGS,
-  history,
   server,
   getDefaultTimeout,
-  onlinePolling,
   printerFileCleanSettingKey,
   getDefaultPrinterFileCleanSettings,
   getDefaultSettings,
