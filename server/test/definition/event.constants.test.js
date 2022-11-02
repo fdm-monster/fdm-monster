ee13688f74845a5def14ec6e3c4d2374725b0166
David Zwart <davidzwa@gmail.com>
2022-11-02 08:50:08 +0100

    test: cover constants a bit

---
+++ server/test/definition/event.constants.test.js
@@ -0,0 +1,7 @@
+const { firmwareFlashUploadEvent } = require("../../constants/event.constants");
+
+describe("event-constants", () => {
+  it("should work for firmware upload", () => {
+    expect(firmwareFlashUploadEvent("asd")).toBe("firmware-upload.asd");
+  });
+});
