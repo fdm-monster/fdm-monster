36d70f704fb88dcd6f829e60785ad0724cbb9b68
David Zwart <davidzwa@gmail.com>
2023-06-03 09:34:57 +0200

    feat: install latest client 1.2.0-rc2

--- server/server.constants.js
+++ server/server.constants.js
@@ -26,7 +26,7 @@ const AppConstants = {
   clientRepoName: "fdm-monster-client",
   serverRepoName: "fdm-monster",
   orgName: "fdm-monster",
-  defaultClientMinimum: "1.1.8",
+  defaultClientMinimum: "1.2.0-rc2",
   serverPath: "./",
 
   influxUrl: "INFLUX_URL",
