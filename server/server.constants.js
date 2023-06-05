4f452ffd42405cc8b49ed8962b7efb8f5a4cf386
David Zwart <davidzwa@gmail.com>
2023-06-05 22:11:33 +0200

    fix: constrain server to client 1.2.0-rc3

--- server/server.constants.js
+++ server/server.constants.js
@@ -26,7 +26,7 @@ const AppConstants = {
   clientRepoName: "fdm-monster-client",
   serverRepoName: "fdm-monster",
   orgName: "fdm-monster",
-  defaultClientMinimum: "1.2.0-rc2",
+  defaultClientMinimum: "1.2.0-rc3",
   serverPath: "./",
 
   influxUrl: "INFLUX_URL",
