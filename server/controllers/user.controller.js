e99afe1c703843efb85e797617bee20b4355e0e8
David Zwart <davidzwa@gmail.com>
2023-08-25 16:28:18 +0200

    feat: add profile to user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -6,26 +6,34 @@ const { validateInput } = require("../handlers/validators");
 const { idRules } = require("./validation/generic.validation");
 
 class UserController {
-  #userService;
+  /**
+   * @type {UserService}
+   */
+  userService;
 
   constructor({ userService }) {
-    this.#userService = userService;
+    this.userService = userService;
+  }
+
+  async profile(req, res) {
+    const user = await this.userService.getUser(req.user.id);
+    res.send(user);
   }
 
   async list(req, res) {
-    const users = await this.#userService.listUsers();
+    const users = await this.userService.listUsers();
     res.send(users);
   }
 
   async delete(req, res) {
     const { id } = await validateInput(req.params, idRules);
-    await this.#userService.deleteUser(id);
+    await this.userService.deleteUser(id);
     res.send();
   }
 
   async get(req, res) {
     const { id } = await validateInput(req.params, idRules);
-    const users = await this.#userService.getUser(id);
+    const users = await this.userService.getUser(id);
     res.send(users);
   }
 }
@@ -34,5 +42,6 @@ module.exports = createController(UserController)
   .prefix(AppConstants.apiRoute + "/user")
   .before([authenticate(), authorizeRoles([ROLES.ADMIN])])
   .get("/", "list")
+  .get("/profile", "profile")
   .get("/:id", "get")
   .delete("/:id", "delete");
