6a3462cd7ec116a0bff197b6ca2e15c19cd1f979
David Zwart <davidzwa@gmail.com>
2023-09-17 19:30:14 +0200

    fix: also upgrade client package

--- src/server.constants.ts
+++ src/server.constants.ts
@@ -61,7 +61,7 @@ export const AppConstants = {
   orgName: "fdm-monster",
   // Wizard version changes will trigger a re-run of the wizard
   currentWizardVersion: 1,
-  defaultClientMinimum: "1.3.1",
+  defaultClientMinimum: "1.3.4",
 
   influxUrl: "INFLUX_URL",
   influxToken: "INFLUX_TOKEN",
