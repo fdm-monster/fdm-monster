9716b87b8ddd6e423e1b4c1732d98ff3984cc79d
David Zwart <davidzwa@gmail.com>
2023-09-18 22:55:34 +0200

    fix: simplify server core imports, remove express-session

--- src/server.core.ts
+++ src/server.core.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import session from "express-session";
+import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import passport from "passport";
-import { scopePerRequest } from "awilix-express";
 import cors from "cors";
 import helmet from "helmet";
+import { asValue } from "awilix";
+import { scopePerRequest } from "awilix-express";
 import { configureContainer } from "./container";
 import { interceptDatabaseError } from "./middleware/database";
-import { validateWhitelistedIp, interceptRoles } from "./middleware/global.middleware";
+import { interceptRoles, validateWhitelistedIp } from "./middleware/global.middleware";
 import { initializePassportStrategies } from "./middleware/passport";
 
 export async function setupServer() {
@@ -27,16 +27,9 @@ export async function setupServer() {
         contentSecurityPolicy: false,
       })
     )
-    .use(express.json({ limit: "10mb" }))
+    .use(json({ limit: "10mb" }))
     .use(cookieParser())
-    .use(express.urlencoded({ extended: false }))
-    .use(
-      session({
-        secret: "supersecret",
-        resave: true,
-        saveUninitialized: true,
-      })
-    )
+    .use(urlencoded({ extended: false }))
     .use(passport.initialize())
     .use(passport.authenticate(["jwt", "anonymous"], { session: false }))
     .use(scopePerRequest(container))
