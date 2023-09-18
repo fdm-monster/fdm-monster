ecf38978e9e17a2786d7841db04d10da6f6b8633
David Zwart <davidzwa@gmail.com>
2023-09-18 22:51:01 +0200

    chore: suppress printing routes in production and add explicit flag

--- src/server.constants.ts
+++ src/server.constants.ts
@@ -86,4 +86,6 @@ export const AppConstants = {
   // Sentry
   sentryCustomDsnToken: "SENTRY_CUSTOM_DSN",
   sentryCustomDsnDefault: "https://164b8028a8a745bba3dbcab991b84ae7@o4503975545733120.ingest.sentry.io/4505101598261248",
+
+  debugRoutesKey: "DEBUG_ROUTES",
 };
