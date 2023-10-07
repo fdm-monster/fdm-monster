d5b48a97da63bb5d5e99c0fba1ff325c4bbb90c1
David Zwart <davidzwa@gmail.com>
2023-10-07 17:12:58 +0200

    fix: cover refresh service even more

--- test/application/refresh-token.service.test.ts
+++ test/application/refresh-token.service.test.ts
@@ -5,14 +5,17 @@ import { configureContainer } from "@/container";
 import { DITokens } from "@/container.tokens";
 import { RefreshToken } from "@/models";
 import { ensureTestUserCreated } from "../api/test-data/create-user";
+import { SettingsStore } from "@/state/settings.store";
 
 let container: AwilixContainer;
 let refreshTokenService: RefreshTokenService;
+let settingsStore: SettingsStore;
 
 beforeAll(async () => {
   await connect();
   container = configureContainer();
   refreshTokenService = container.resolve<RefreshTokenService>(DITokens.refreshTokenService);
+  settingsStore = container.resolve<SettingsStore>(DITokens.settingsStore);
 });
 afterEach(async () => {
   await RefreshToken.deleteMany();
@@ -26,8 +29,16 @@ describe(RefreshTokenService.name, () => {
     await refreshTokenService.purgeAllOutdatedRefreshTokens();
   });
 
-  it("should purge refresh tokens", async () => {
+  it("should purge refresh tokens of user", async () => {
     const user = await ensureTestUserCreated();
     await refreshTokenService.purgeOutdatedRefreshTokensByUserId(user.id);
+  });
+
+  it("should delete refresh token", async () => {
+    await settingsStore.loadSettings();
+    const user = await ensureTestUserCreated("test", "test");
+    const refreshToken = await refreshTokenService.createRefreshTokenForUserId(user.id);
+    await refreshTokenService.deleteRefreshToken(refreshToken);
+    expect(await refreshTokenService.getRefreshToken(refreshToken, false)).toBeNull();
   });
 });
