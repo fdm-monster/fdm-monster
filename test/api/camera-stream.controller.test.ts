f2a43c19a268ecfef5e89d20ab6d91142efc345e
David Zwart <davidzwa@gmail.com>
2023-10-07 14:37:04 +0200

    test: cover camera api

--- test/api/camera-stream.controller.test.ts
+++ test/api/camera-stream.controller.test.ts
@@ -4,6 +4,7 @@ import { expectOkResponse } from "../extensions";
 import { AppConstants } from "@/server.constants";
 import { CameraStream as Model } from "@/models";
 import supertest from "supertest";
+import { createTestPrinter } from "./test-data/create-printer";
 
 const listRoute = `${AppConstants.apiRoute}/camera-stream`;
 const getRoute = (id: string) => `${listRoute}/${id}`;
@@ -40,8 +41,11 @@ describe("CameraStreamController", () => {
     printerId: null,
     settings: defaultSettings,
   });
+  const getTestCameraStream = async (id: string) => await request.get(getRoute(id));
   const createTestCameraStream = async (url: string) => await request.post(listRoute).send(defaultCameraStreamInput(url));
   const deleteTestCameraStream = async (id: string) => await request.delete(deleteRoute(id));
+  const updateTestCameraStream = async (id: string, url: string, printerId: string | null) =>
+    await request.delete(updateRoute(id)).send(defaultCameraStreamInput(url));
 
   it("should list streams", async () => {
     const res = await request.get(listRoute);
@@ -52,6 +56,7 @@ describe("CameraStreamController", () => {
   it("should create stream", async () => {
     const res = await createTestCameraStream(defaultTestURL);
     expectOkResponse(res, matchedBody(defaultTestURL));
+    expectOkResponse(await getTestCameraStream(res.body.id), matchedBody(defaultTestURL));
   });
 
   it("should create two streams with null printerId", async () => {
@@ -66,5 +71,13 @@ describe("CameraStreamController", () => {
     await expectOkResponse(res, matchedBody(defaultTestURL + "4"));
     const deleteResponse = await deleteTestCameraStream(res.body.id);
     await expectOkResponse(deleteResponse);
+  });
+
+  it("should create and update stream", async () => {
+    const res = await createTestCameraStream(defaultTestURL + "5");
+    await expectOkResponse(res, matchedBody(defaultTestURL + "5"));
+    const printer = await createTestPrinter(request, true);
+    const updateResponse = await updateTestCameraStream(res.body.id, defaultTestURL + "6", printer.id);
+    await expectOkResponse(updateResponse);
   });
 });
--- test/api/test-data/create-printer.ts
+++ test/api/test-data/create-printer.ts
@@ -1,10 +1,11 @@
 import { AppConstants } from "@/server.constants";
 import { expectOkResponse } from "../../extensions";
+import supertest from "supertest";
 
 const printerRoute = AppConstants.apiRoute + "/printer";
 export const testApiKey = "fdmonsterfdmonsterfdmonsterfdmon";
 
-export async function createTestPrinter(request, enabled = false) {
+export async function createTestPrinter(request: supertest.SuperTest<supertest.Test>, enabled = false) {
   const createResponse = await request.post(printerRoute).send({
     printerURL: "http://url.com",
     apiKey: testApiKey,
