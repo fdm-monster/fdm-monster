a69a5336034a683c62900c6d7bf1af25e206f7db
David Zwart <davidzwa@gmail.com>
2023-10-14 10:15:19 +0200

    fix: implement missing sqlite methods for settings

--- src/services/orm/settings.service.ts
+++ src/services/orm/settings.service.ts
@@ -8,6 +8,7 @@ import {
   getDefaultFrontendSettings,
   getDefaultServerSettings,
   getDefaultTimeout,
+  getDefaultWhitelistIpAddresses,
   getDefaultWizardSettings,
   serverSettingsKey,
   timeoutSettingKey,
@@ -26,10 +27,22 @@ import {
 import { SqliteIdType } from "@/shared.constants";
 import { ISettingsService } from "@/services/interfaces/settings.service.interface";
 import { Error, Promise } from "mongoose";
-import { ICredentialSettings, ITimeoutSettings } from "@/models/Settings";
-import { NotImplementedException } from "@/exceptions/runtime.exceptions";
+import { ICredentialSettings } from "@/models/Settings";
+import { BadRequestException, NotImplementedException } from "@/exceptions/runtime.exceptions";
+import { AppConstants } from "@/server.constants";
+import { IConfigService } from "@/services/core/config.service";
+import { TypeormService } from "@/services/typeorm/typeorm.service";
+import { validateInput } from "@/handlers/validators";
+import { whitelistSettingUpdateRules } from "@/services/validators/settings-service.validation";
 
 export class SettingsService2 extends BaseService(Settings, SettingsDto) implements ISettingsService<SqliteIdType, Settings> {
+  configService: IConfigService;
+
+  constructor({ configService, typeormService }: { configService: IConfigService; typeormService: TypeormService }) {
+    super({ typeormService });
+    this.configService = configService;
+  }
+
   toDto(entity: Settings): SettingsDto<SqliteIdType> {
     return {
       [serverSettingsKey]: entity[serverSettingsKey],
@@ -41,10 +54,10 @@ export class SettingsService2 extends BaseService(Settings, SettingsDto) impleme
   }
 
   async getOrCreate() {
-    const settings = await this.get();
+    let settings = await this.get();
 
     if (!settings) {
-      const settings = this.create({
+      const settings = await this.create({
         [serverSettingsKey]: getDefaultServerSettings(),
         [credentialSettingsKey]: getDefaultCredentialSettings(),
         [wizardSettingKey]: getDefaultWizardSettings(),
@@ -53,6 +66,8 @@ export class SettingsService2 extends BaseService(Settings, SettingsDto) impleme
         [timeoutSettingKey]: getDefaultTimeout(),
       });
       return settings;
+    } else {
+      settings = this.migrateSettingsRuntime(settings);
     }
 
     return settings;
@@ -138,31 +153,107 @@ export class SettingsService2 extends BaseService(Settings, SettingsDto) impleme
     return settingsList?.length ? settingsList[0] : null;
   }
 
-  migrateSettingsRuntime(knownSettings: Partial<Settings>): any {
-    throw new NotImplementedException();
+  migrateSettingsRuntime(knownSettings: Settings): Settings {
+    const entity = knownSettings; // alias _doc also works
+    if (!entity[fileCleanSettingKey]) {
+      entity[fileCleanSettingKey] = getDefaultFileCleanSettings();
+    }
+
+    // Server settings exist, but need updating with new ones if they don't exist.
+    if (!entity[wizardSettingKey]) {
+      entity[wizardSettingKey] = getDefaultWizardSettings();
+    }
+    if (!entity[timeoutSettingKey]) {
+      entity[timeoutSettingKey] = getDefaultTimeout();
+    }
+    if (!entity[serverSettingsKey]) {
+      entity[serverSettingsKey] = getDefaultServerSettings();
+    }
+    if (!entity[credentialSettingsKey]) {
+      entity[credentialSettingsKey] = getDefaultCredentialSettings();
+    }
+    if (!this.isWhiteListSettingEnabled()) {
+      entity.server.whitelistEnabled = false;
+      entity.server.whitelistedIpAddresses = getDefaultWhitelistIpAddresses();
+    } else if (!entity.server.whitelistedIpAddresses?.length) {
+      entity.server.whitelistedIpAddresses = getDefaultWhitelistIpAddresses();
+    }
+    if (!entity[frontendSettingKey]) {
+      entity[frontendSettingKey] = getDefaultFrontendSettings();
+    }
+
+    return entity;
   }
 
-  patchCredentialSettings(patchUpdate: Partial<ICredentialSettings>): Promise<Settings> {
-    throw new NotImplementedException();
+  async patchCredentialSettings(patchUpdate: Partial<ICredentialSettings>): Promise<Settings> {
+    const entity = await this.getOrCreate();
+    if (!entity[credentialSettingsKey]) {
+      throw new Error("No existing wizard settings found, cant patch");
+    }
+
+    const newCredentialSettings = {
+      ...entity[credentialSettingsKey],
+      ...patchUpdate,
+    };
+
+    return await this.updateCredentialSettings(newCredentialSettings);
   }
 
-  patchFileCleanSettings(fileClean: Partial<FileCleanSettingsDto>): Promise<Settings> {
-    throw new NotImplementedException();
+  async patchFileCleanSettings(patchUpdate: Partial<FileCleanSettingsDto>): Promise<Settings> {
+    const entity = await this.getOrCreate();
+    if (!entity[fileCleanSettingKey]) {
+      throw new Error("No existing wizard settings found, cant patch");
+    }
+
+    const newFileCleanSettings = {
+      ...entity[fileCleanSettingKey],
+      ...patchUpdate,
+    };
+
+    return await this.updateFileCleanSettings(newFileCleanSettings);
   }
 
-  setLoginRequired(enabled: boolean): Promise<Settings> {
-    throw new NotImplementedException();
+  async setLoginRequired(enabled: boolean): Promise<Settings> {
+    const entity = await this.getOrCreate();
+    entity[serverSettingsKey].loginRequired = enabled;
+    await this.update(entity.id, entity);
+    return entity;
   }
 
-  setRegistrationEnabled(enabled: boolean): Promise<Settings> {
-    throw new NotImplementedException();
+  async setRegistrationEnabled(enabled: boolean): Promise<Settings> {
+    const entity = await this.getOrCreate();
+    entity[serverSettingsKey].registration = enabled;
+    await this.update(entity.id, entity);
+    return entity;
   }
 
-  setSentryDiagnosticsEnabled(enabled: boolean): Promise<Settings> {
-    throw new NotImplementedException();
+  async setSentryDiagnosticsEnabled(enabled: boolean): Promise<Settings> {
+    const entity = await this.getOrCreate();
+    entity[serverSettingsKey].sentryDiagnosticsEnabled = enabled;
+    await this.update(entity.id, entity);
+    return entity;
   }
 
-  setWhitelist(enabled: boolean, ipAddresses: string[]): Promise<Settings> {
-    throw new NotImplementedException();
+  async setWhitelist(enabled: boolean, ipAddresses: string[]): Promise<Settings> {
+    if (!this.isWhiteListSettingEnabled()) {
+      throw new BadRequestException("Whitelist settings are not enabled");
+    }
+    await validateInput(
+      {
+        enabled,
+        ipAddresses,
+      },
+      whitelistSettingUpdateRules
+    );
+
+    const entity = await this.getOrCreate();
+    entity[serverSettingsKey].whitelistEnabled = enabled;
+    entity[serverSettingsKey].whitelistedIpAddresses = ipAddresses;
+    await this.update(entity.id, entity);
+    return entity;
+  }
+
+  isWhiteListSettingEnabled() {
+    return this.configService.get(AppConstants.ENABLE_EXPERIMENTAL_WHITELIST_SETTINGS, "false") === "true";
   }
 }
