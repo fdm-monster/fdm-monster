3a508d4bee5e3064308dcf1182b40b8d0b8b1945
David Zwart <davidzwa@gmail.com>
2023-11-27 22:16:00 +0100

    fix: connect db before test

--- test/api/host/boot-task.test.ts
+++ test/api/host/boot-task.test.ts
@@ -12,6 +12,7 @@ let serverTasks: ServerTasks;
 let task: BootTask;
 
 beforeAll(async () => {
+  await connect();
   ({ container } = await setupTestApp(false, undefined, true));
   taskManager = container.resolve(DITokens.taskManagerService);
   task = container.resolve(DITokens.bootTask);
