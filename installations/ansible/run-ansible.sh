507704231ae8ecab816c5292295e7975ba0dbea5
David Zwart <davidzwa@gmail.com>
2023-12-02 17:27:30 +0100

    fix: tweak titles and add mongodb to playbook runner script

--- installations/ansible/run-ansible.sh
+++ installations/ansible/run-ansible.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 inventory=inventory.cfg
+playbook_mongo=playbook-mongodb6.yaml
 playbook=playbook-ubuntu22.yaml
 
-ansible-playbook -i $inventory $playbook $1 \
+ansible-playbook -i $inventory $playbook_mongo $1
+ansible-playbook -i $inventory $playbook $1
