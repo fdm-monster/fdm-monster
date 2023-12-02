98601a5ce39a854b879a0d0af04e9ad3205ae9d7
David Zwart <davidzwa@gmail.com>
2023-12-02 18:32:36 +0100

    fix: cleanup

--- installations/ansible/run-ansible.sh
+++ installations/ansible/run-ansible.sh
@@ -3,5 +3,5 @@ inventory=inventory.cfg
 playbook_mongo=playbook-mongodb6.yaml
 playbook=playbook-ubuntu22.yaml
 
-ansible-playbook -i $inventory $playbook_mongo $1
-ansible-playbook -i $inventory $playbook $1
+ansible-playbook -i $inventory $playbook_mongo $1 $2
+ansible-playbook -i $inventory $playbook $1 $2
--- installations/ansible/test-ansible.sh
+++ installations/ansible/test-ansible.sh
@@ -3,6 +3,6 @@ inventory=inventory.cfg
 host=fdmm
 
 echo "Listing ansible hosts from inventory file '$inventory'"
-ansible -i $inventory $host --list-hosts $1
-ansible -i $inventory $host -m ping $1
-ansible -i $inventory $host -a 'free -h' $1
+ansible -i $inventory $host --list-hosts $1 $2
+ansible -i $inventory $host -m ping $1 $2
+ansible -i $inventory $host -a 'free -h' $1 $2
