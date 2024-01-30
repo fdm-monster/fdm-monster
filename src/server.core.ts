45b2757d707783ce664dcd2c2383b118a20852df
David Zwart <davidzwa@gmail.com>
2024-01-30 21:44:01 +0100

    feat: use different env flag to enforce sqlite (default mongodb) during tests

--- src/server.core.ts
+++ src/server.core.ts
@@ -16,7 +16,7 @@ import { getEnvOrDefault } from "@/utils/env.utils";
 export async function setupServer() {
   const httpServer = express();
   const experimentalTypeormEnabled =
-    getEnvOrDefault(AppConstants.enableExperimentalTypeormKey, AppConstants.enableExperimentalTypeormDefault) === "true";
+    getEnvOrDefault(AppConstants.ENABLE_EXPERIMENTAL_TYPEORM, AppConstants.enableExperimentalTypeormDefault) === "true";
   if (experimentalTypeormEnabled) {
     const dbFolder = process.env[AppConstants.DATABASE_PATH] || "./database";
     ensureDirExists(join(superRootPath(), dbFolder));
--- test/setup-global.ts
+++ test/setup-global.ts
@@ -1,10 +1,10 @@
 import { AppConstants } from "@/server.constants";
+
 module.exports = async () => {
   process.env.TZ = "UTC";
   process.env[AppConstants.VERSION_KEY] = "1.0.0";
   process.env[AppConstants.ENABLE_EXPERIMENTAL_WHITELIST_SETTINGS] = "true";
   process.env[AppConstants.OVERRIDE_IS_DEMO_MODE] = "false";
   process.env[AppConstants.DATABASE_FILE] = ":memory:";
-  process.env[AppConstants.enableExperimentalTypeormKey] = "true";
-  process.env[AppConstants.OVERRIDE_IS_DEMO_MODE] = "false";
+  process.env[AppConstants.ENABLE_EXPERIMENTAL_TYPEORM] = process.env["SQLITE_MODE"] || "false";
 };
