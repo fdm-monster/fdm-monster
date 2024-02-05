abc4236b646d257f89123ff938ca2ba21a5dcc56
David Zwart <david@visionintellilgence.ai>
2024-02-05 23:17:33 +0100

    release: 1.6.0-rc3

--- src/server.constants.ts
+++ src/server.constants.ts
@@ -68,7 +68,7 @@ export const AppConstants = {
   orgName: "fdm-monster",
   // Wizard version changes will trigger a re-run of the wizard
   currentWizardVersion: 1,
-  defaultClientMinimum: "1.5.0-rc4",
+  defaultClientMinimum: "1.5.0-rc8",
 
   influxUrl: "INFLUX_URL",
   influxToken: "INFLUX_TOKEN",
