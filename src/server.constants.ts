790fe65b20f266e25d0df8ded51805af927b36b2
David Zwart <davidzwa@gmail.com>
2024-07-07 15:19:04 +0200

    change: install client 1.6.0

--- src/server.constants.ts
+++ src/server.constants.ts
@@ -69,7 +69,7 @@ export const AppConstants = {
   orgName: "fdm-monster",
   // Wizard version changes will trigger a re-run of the wizard
   currentWizardVersion: 1,
-  defaultClientMinimum: "1.5.14",
+  defaultClientMinimum: "1.6.0",
 
   influxUrl: "INFLUX_URL",
   influxToken: "INFLUX_TOKEN",
