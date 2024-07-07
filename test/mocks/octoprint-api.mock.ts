2ed19d6ff1d33ca86ce5d70bb3737a18f7d8cfff
David Zwart <davidzwa@gmail.com>
2024-07-07 15:56:07 +0200

    typing

--- test/mocks/octoprint-api.mock.ts
+++ test/mocks/octoprint-api.mock.ts
@@ -1,10 +1,24 @@
 import { OctoprintClient } from "@/services/octoprint/octoprint.client";
 import { AxiosMock } from "./axios.mock";
+import { SettingsStore } from "@/state/settings.store";
+import { AxiosStatic } from "axios";
+import { ILoggerFactory } from "@/handlers/logger-factory";
+import EventEmitter2 from "eventemitter2";
 
 export class OctoPrintApiMock extends OctoprintClient {
   eventEmitter2;
 
-  constructor({ settingsStore, httpClient, loggerFactory, eventEmitter2 }) {
+  constructor({
+    settingsStore,
+    httpClient,
+    loggerFactory,
+    eventEmitter2,
+  }: {
+    settingsStore: SettingsStore;
+    httpClient: AxiosStatic;
+    loggerFactory: ILoggerFactory;
+    eventEmitter2: EventEmitter2;
+  }) {
     super({ settingsStore, httpClient, loggerFactory, eventEmitter2 });
     this.eventEmitter2 = eventEmitter2;
     this.logger = loggerFactory(OctoPrintApiMock.name, false);
