3db78f93a2f811c4ed104661368f31939c584b26
David Zwart <davidzwa@gmail.com>
2024-11-01 17:18:46 +0100

    feat: check setting in socket factory

--- src/services/socket.factory.ts
+++ src/services/socket.factory.ts
@@ -2,6 +2,7 @@ import { DITokens } from "@/container.tokens";
 import { OctoprintWebsocketAdapter } from "@/services/octoprint/octoprint-websocket.adapter";
 import { MoonrakerWebsocketAdapter } from "@/services/moonraker/moonraker-websocket.adapter";
 import { MoonrakerType, OctoprintType } from "@/services/printer-api.interface";
+import { SettingsStore } from "@/state/settings.store";
 
 export class SocketFactory {
   cradle: any;
@@ -11,9 +12,13 @@ export class SocketFactory {
   }
 
   createInstance(printerType: number): OctoprintWebsocketAdapter | MoonrakerWebsocketAdapter {
+    const settingsStore = this.cradle[DITokens.settingsStore] as SettingsStore;
+    const serverSettings = settingsStore.getServerSettings();
+    const moonrakerSupport = serverSettings.experimentalMoonrakerSupport;
+
     if (printerType === OctoprintType) {
       return this.cradle[DITokens.octoPrintSockIoAdapter];
-    } else if (printerType === MoonrakerType) {
+    } else if (moonrakerSupport && printerType === MoonrakerType) {
       return this.cradle[DITokens.moonrakerWebsocketAdapter];
     } else {
       throw new Error("PrinterType is unknown, cant pick the right socket adapter");
