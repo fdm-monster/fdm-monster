fa5996f26c5ca5badd5447ac5f024c456b667cb7
David Zwart <davidzwa@gmail.com>
2024-11-01 17:57:39 +0100

    release: 1.7.0

--- src/server.constants.ts
+++ src/server.constants.ts
@@ -71,7 +71,7 @@ export const AppConstants = {
   orgName: "fdm-monster",
   // Wizard version changes will trigger a re-run of the wizard
   currentWizardVersion: 1,
-  defaultClientMinimum: "1.6.4",
+  defaultClientMinimum: "1.6.5",
 
   influxUrl: "INFLUX_URL",
   influxToken: "INFLUX_TOKEN",
