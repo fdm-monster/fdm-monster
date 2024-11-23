dd8ece8b4142efef9582e78d97c31ea36ec80b7e
David Zwart <davidzwa@gmail.com>
2024-11-23 18:08:10 +0100

    fix: GUEST should not have access to change cameras

--- src/controllers/camera-stream.controller.ts
+++ src/controllers/camera-stream.controller.ts
@@ -1,13 +1,17 @@
-import { createController } from "awilix-express";
+import { route, GET, POST, PUT, DELETE, before } from "awilix-router-core";
 import { AppConstants } from "@/server.constants";
 import { validateInput } from "@/handlers/validators";
 import { idRulesV2 } from "./validation/generic.validation";
-import { authenticate } from "@/middleware/authenticate";
+import { authenticate, authorizeRoles } from "@/middleware/authenticate";
 import { Request, Response } from "express";
 import { ICameraStreamService } from "@/services/interfaces/camera-stream.service.interface";
+import { ROLES } from "@/constants/authorization.constants";
+import { printerResolveMiddleware } from "@/middleware/printer";
 
+@route(`${AppConstants.apiRoute}/camera-stream`)
+@before([authenticate(), authorizeRoles([ROLES.OPERATOR, ROLES.ADMIN]), printerResolveMiddleware()])
 export class CameraStreamController {
-  private cameraStreamService: ICameraStreamService;
+  private readonly cameraStreamService: ICameraStreamService;
   private readonly isTypeormMode: boolean;
 
   constructor({ cameraStreamService, isTypeormMode }: { cameraStreamService: ICameraStreamService; isTypeormMode: boolean }) {
@@ -15,40 +19,41 @@ export class CameraStreamController {
     this.isTypeormMode = isTypeormMode;
   }
 
+  @GET()
+  @route("/")
   async list(req: Request, res: Response) {
     const result = await this.cameraStreamService.list();
     res.send(result.map((item) => this.cameraStreamService.toDto(item)));
   }
 
+  @GET()
+  @route("/:id")
   async get(req: Request, res: Response) {
     const { id } = await validateInput(req.params, idRulesV2(this.isTypeormMode));
     const result = await this.cameraStreamService.get(id);
     res.send(this.cameraStreamService.toDto(result));
   }
 
+  @POST()
+  @route("/")
   async create(req: Request, res: Response) {
     const result = await this.cameraStreamService.create(req.body);
     res.send(this.cameraStreamService.toDto(result));
   }
 
+  @PUT()
+  @route("/:id")
   async update(req: Request, res: Response) {
     const { id } = await validateInput(req.params, idRulesV2(this.isTypeormMode));
     const result = await this.cameraStreamService.update(id, req.body);
     res.send(this.cameraStreamService.toDto(result));
   }
 
+  @DELETE()
+  @route("/:id")
   async delete(req: Request, res: Response) {
     const { id } = await validateInput(req.params, idRulesV2(this.isTypeormMode));
     await this.cameraStreamService.delete(id);
     res.send();
   }
 }
-
-export default createController(CameraStreamController)
-  .prefix(AppConstants.apiRoute + "/camera-stream")
-  .before([authenticate()])
-  .get("/", "list")
-  .get("/:id", "get")
-  .post("/", "create")
-  .delete("/:id", "delete")
-  .put("/:id", "update");
