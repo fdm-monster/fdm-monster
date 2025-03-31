4e7483dc90a07746d00793931a6db189ea71d7ff
David Zwart <davidzwa@gmail.com>
2025-03-31 23:31:54 +0200

    chore

--- src/services/validators/camera-service.validation.ts
+++ src/services/validators/camera-service.validation.ts
@@ -4,11 +4,6 @@ import { idRuleV2 } from "@/controllers/validation/generic.validation";
 export const createCameraStreamSchema = (isSqlite: boolean) =>
   z.object({
     printerId: idRuleV2(isSqlite).nullish(),
-    streamURL: z
-      .string()
-      .url("Invalid URL format")
-      .refine((val) => /^https?:\/\//.test(val), {
-        message: "Stream URL must be a valid HTTP or HTTPS URL",
-      }),
-    name: z.string().min(1, "Name is required").optional(),
+    streamURL: z.string().url(),
+    name: z.string().min(1).optional(),
   });
