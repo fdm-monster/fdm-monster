6532d6d01aff51cb134104a158a1544d3633f8bf
David Zwart <davidzwa@gmail.com>
2025-03-31 23:39:15 +0200

    fix: registering user should not require rold

--- src/services/validators/user-service.validation.ts
+++ src/services/validators/user-service.validation.ts
@@ -4,10 +4,10 @@ import { idRuleV2 } from "@/controllers/validation/generic.validation";
 
 export const registerUserSchema = (isSqlite: boolean) =>
   z.object({
-    username: z.string().min(AppConstants.DEFAULT_USERNAME_MINLEN), // Ensures username length
-    password: z.string().min(AppConstants.DEFAULT_PASSWORD_MINLEN), // Ensures password length
+    username: z.string().min(AppConstants.DEFAULT_USERNAME_MINLEN),
+    password: z.string().min(AppConstants.DEFAULT_PASSWORD_MINLEN),
     needsPasswordChange: z.boolean().optional(),
-    roles: z.array(idRuleV2(isSqlite)).min(1), // Ensure at least one role is provided
+    roles: z.array(idRuleV2(isSqlite)),
     isDemoUser: z.boolean().optional(),
     isRootUser: z.boolean().optional(),
     isVerified: z.boolean().optional(),
