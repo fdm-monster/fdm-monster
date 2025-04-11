71c8287840ae5e306fcda13bdc42b17633a9728a
David Zwart <davidzwa@gmail.com>
2025-04-11 13:08:48 +0200

    fix: cleanup socket io gateway authorization typing

--- src/state/socket-io.gateway.ts
+++ src/state/socket-io.gateway.ts
@@ -7,37 +7,8 @@ import { ILoggerFactory } from "@/handlers/logger-factory";
 import { Server as HttpServer } from "http";
 import { getPassportJwtOptions, verifyUserCallback } from "@/middleware/passport";
 import { IConfigService } from "@/services/core/config.service";
-import { Strategy as JwtStrategy, StrategyOptions, VerifiedCallback } from "passport-jwt";
-import { DefaultEventsMap } from "socket.io/dist/typed-events";
-import { ExtendedError } from "socket.io/dist/namespace";
 import { IUserService } from "@/services/interfaces/user-service.interface";
-
-const authorize = (
-  settingsStore: SettingsStore,
-  options: StrategyOptions,
-  verify: (jwt_payload: any, done: VerifiedCallback) => void,
-) => {
-  const strategy = new JwtStrategy(options, verify);
-
-  return async function authorizeCallback(
-    socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
-    next: (err?: ExtendedError) => void,
-  ) {
-    if (!(await settingsStore.getLoginRequired())) {
-      // No login required, so we can skip the authentication
-      return next();
-    }
-    // --- Begin strategy augmentation ala passport
-    strategy.success = function success(user) {
-      socket.handshake.user = user;
-      next();
-    };
-    strategy.fail = (info) => next(new Error(info));
-    strategy.error = (error) => next(error);
-
-    strategy.authenticate(socket, {});
-  };
-};
+import { authorize } from "@/middleware/socketio.middleware";
 
 export class SocketIoGateway {
   logger: LoggerService;
@@ -49,7 +20,7 @@ export class SocketIoGateway {
     private readonly eventEmitter2: EventEmitter2,
     private readonly settingsStore: SettingsStore,
     private readonly userService: IUserService,
-    private readonly configService: IConfigService,
+    private readonly configService: IConfigService
   ) {
     this.logger = loggerFactory(SocketIoGateway.name);
   }
@@ -59,10 +30,9 @@ export class SocketIoGateway {
     const opts = getPassportJwtOptions(
       this.settingsStore,
       this.configService,
-      (value: Socket) => value.handshake.auth.token,
+      (value: Socket) => value.handshake.auth.token
     );
-    const verify = verifyUserCallback(this.userService);
-    this.io.use(authorize(this.settingsStore, opts, verify));
+    this.io.use(authorize(this.settingsStore, opts, verifyUserCallback(this.userService)));
     this.io.on("connection", (socket) => this.onConnect.bind(this)(socket));
   }
 
@@ -91,5 +61,5 @@ export const IO_MESSAGES = {
   LegacyPrinterTest: "legacy-printer-test",
   CompletionEvent: "completion-event",
   HostState: "host-state",
-  ApiAccessibility: "api-accessibility",
+  ApiAccessibility: "api-accessibility"
 };
