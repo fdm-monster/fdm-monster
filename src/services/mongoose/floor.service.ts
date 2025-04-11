32506f49f3eedfcd5ff1461d468f22d7ea2de609
David Zwart <davidzwa@gmail.com>
2025-04-11 10:52:06 +0200

    fix: lingering throwError removed floorService, 202 errors left

--- src/services/mongoose/floor.service.ts
+++ src/services/mongoose/floor.service.ts
@@ -2,11 +2,11 @@ import { Floor, IFloor } from "@/models/Floor";
 import { validateInput } from "@/handlers/validators";
 import { NotFoundException } from "@/exceptions/runtime.exceptions";
 import {
+  createOrUpdateFloorSchema,
   printerInFloorSchema,
   removePrinterInFloorSchema,
-  updateFloorNameSchema,
   updateFloorLevelSchema,
-  createOrUpdateFloorSchema,
+  updateFloorNameSchema
 } from "../validators/floor-service.validation";
 import { PrinterCache } from "@/state/printer.cache";
 import { MongoIdType } from "@/shared.constants";
@@ -14,7 +14,8 @@ import { IFloorService } from "@/services/interfaces/floor.service.interface";
 import { CreateFloorDto, FloorDto, PositionDto, UpdateFloorDto } from "@/services/interfaces/floor.dto";
 
 export class FloorService implements IFloorService<MongoIdType> {
-  constructor(private readonly printerCache: PrinterCache) {}
+  constructor(private readonly printerCache: PrinterCache) {
+  }
 
   toDto(floor: IFloor): FloorDto<MongoIdType> {
     return {
@@ -25,8 +26,8 @@ export class FloorService implements IFloorService<MongoIdType> {
         x: p.x,
         y: p.y,
         printerId: p.printerId.toString(),
-        floorId: floor.id,
-      })),
+        floorId: floor.id
+      }))
     };
   }
 
@@ -34,9 +35,9 @@ export class FloorService implements IFloorService<MongoIdType> {
     return Floor.find({});
   }
 
-  async get(floorId: MongoIdType, throwError = true) {
+  async get(floorId: MongoIdType) {
     const floor = await Floor.findOne({ _id: floorId });
-    if (!floor && throwError) {
+    if (!floor) {
       throw new NotFoundException(`Floor with provided id does not exist`);
     }
 
@@ -47,7 +48,7 @@ export class FloorService implements IFloorService<MongoIdType> {
     return await this.create({
       name: "Default Floor",
       floor: 1,
-      printers: [],
+      printers: []
     });
   }
 
@@ -70,7 +71,7 @@ export class FloorService implements IFloorService<MongoIdType> {
       ...existingFloor,
       name: update.name,
       printers: update.printers,
-      floor: update.floor,
+      floor: update.floor
     };
     const input = await validateInput(floorUpdate, createOrUpdateFloorSchema(false));
 
@@ -103,16 +104,16 @@ export class FloorService implements IFloorService<MongoIdType> {
         $pull: {
           printers: {
             printerId: {
-              $in: [printerId],
-            },
-          },
-        },
-      },
+              $in: [printerId]
+            }
+          }
+        }
+      }
     );
   }
 
   async addOrUpdatePrinter(floorId: MongoIdType, updatedPosition: PositionDto<MongoIdType>) {
-    const floor = await this.get(floorId, true);
+    const floor = await this.get(floorId);
 
     updatedPosition.floorId = floor.id;
     const validInput = await validateInput(updatedPosition, printerInFloorSchema(false));
@@ -124,7 +125,7 @@ export class FloorService implements IFloorService<MongoIdType> {
     floor.printers = floor.printers.filter((position) => !(position.x === validInput.x && position.y === validInput.y));
 
     const positionIndex = floor.printers.findIndex(
-      (position) => position.printerId.toString() === validInput.printerId.toString(),
+      (position) => position.printerId.toString() === validInput.printerId.toString()
     );
     if (positionIndex !== -1) {
       floor.printers[positionIndex] = validInput as PositionDto<MongoIdType>;
@@ -137,14 +138,14 @@ export class FloorService implements IFloorService<MongoIdType> {
   }
 
   async removePrinter(floorId: MongoIdType, printerId: MongoIdType) {
-    const floor = await this.get(floorId, true);
+    const floor = await this.get(floorId);
     const validInput = await validateInput({ printerId }, removePrinterInFloorSchema(false));
 
     // Ensure printer exists
     await this.printerCache.getCachedPrinterOrThrowAsync(validInput.printerId);
 
     const foundPrinterInFloorIndex = floor.printers.findIndex(
-      (pif) => pif.printerId.toString() === validInput.printerId.toString(),
+      (pif) => pif.printerId.toString() === validInput.printerId.toString()
     );
     if (foundPrinterInFloorIndex === -1) return floor;
     floor.printers.splice(foundPrinterInFloorIndex, 1);
@@ -153,7 +154,7 @@ export class FloorService implements IFloorService<MongoIdType> {
   }
 
   async delete(floorId: MongoIdType) {
-    await this.get(floorId, true);
+    await this.get(floorId);
     await Floor.deleteOne({ _id: floorId });
   }
 }
