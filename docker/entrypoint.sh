b7c4be4aad15b368ff4eb33e4715e3d998128cec
That Guy Jaysen <thatguy@jaysenodell.com>
2026-01-13 14:08:44 -0500

    Added mdns support to Docker setup
    docker/Dockerfile-mdns: Added avahi-daemon installation and configuration for mDNS support.
    docker/entrypoint.sh: Started avahi-daemon in the background before executing the main process.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,4 +12,9 @@ else
     echo "Media folder already exists..."
 fi
 
+# mDNS support ... starting the avahi-daemon
+echo "Starting mDNS support via avahi-daemon..."
+avahi-daemon --no-chroot --daemonize -f /etc/avahi/avahi-daemon.conf
+
+
 pm2 start dist/index.js --name FDM --no-daemon -o './media/logs/pm2.log' -e './media/logs/pm2.error.log' --time --restart-delay=1000 --exp-backoff-restart-delay=1500
