15a6f5089cd080fba23bdf94760d55f7103721c3
davidzwa <davidzwa@gmail.com>
2026-01-15 09:13:54 +0100

    chore: upgrade client

--- src/server.constants.ts
+++ src/server.constants.ts
@@ -73,7 +73,7 @@ export const AppConstants = {
   orgName: "fdm-monster",
   // Wizard version changes will trigger a re-run of the wizard
   currentWizardVersion: 1,
-  defaultClientMinimum: "2.2.3",
+  defaultClientMinimum: "2.2.4",
 
   // Websocket values
   defaultWebsocketHandshakeTimeout: 3000,
