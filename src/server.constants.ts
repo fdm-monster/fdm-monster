797e6cf33b78c26ed92a21419d3b3368411f2461
davidzwa <davidzwa@gmail.com>
2026-02-01 17:19:36 +0100

    fix: version

--- src/server.constants.ts
+++ src/server.constants.ts
@@ -73,7 +73,7 @@ export const AppConstants = {
   orgName: "fdm-monster",
   // Wizard version changes will trigger a re-run of the wizard
   currentWizardVersion: 1,
-  defaultClientMinimum: "2.3.1",
+  defaultClientMinimum: "2.3.3",
 
   // Websocket values
   defaultWebsocketHandshakeTimeout: 3000,
